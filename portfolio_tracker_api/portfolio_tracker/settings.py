006bc41703f77bf35a00e4e53449926610ca4fcb
joha7809 <joha7809@gmail.com>
2025-04-18 15:16:47 +0100

    Added custom user Model for auth

--- portfolio_tracker_api/portfolio_tracker/settings.py
+++ portfolio_tracker_api/portfolio_tracker/settings.py
@@ -108,6 +108,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#Auth user model
+
+AUTH_USER_MODEL = 'user.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- portfolio_tracker_api/user/models.py
+++ portfolio_tracker_api/user/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+class User(AbstractUser):
+    #Open to extension if needed
+    pass
